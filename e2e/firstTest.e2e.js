c67ca8e0ce98d210fe43ec74c59a5ea00277c7f6
Pedro Palacios Avila <wesitos@makerlabperu.org>
2022-07-27 10:59:49 -0500

    android wip

--- e2e/firstTest.e2e.js
+++ e2e/firstTest.e2e.js
@@ -1,6 +1,44 @@
+const METRO_URL = 'http://localhost:8081'
+
+const handleDevClientReuse = async () => {
+  try {
+    await element(by.text('Enter URL manually')).tap();
+  } catch {
+    // We're not using dev-client
+    return;
+  }
+
+  await element(by.id('DevLauncherURLInput')).typeText(METRO_URL);
+  await element(by.text('Connect')).tap();
+};
+
+const handleDevClient = async () => {
+  try {
+    await expect(element(by.text('Enter URL manually'))).toBeVisible();
+  } catch {
+    // We're not using dev-client
+    return;
+  }
+
+  try {
+    await element(by.text(METRO_URL)).tap();
+  } catch {
+    await handleDevClientReuse();
+    return;
+  }
+
+  await element(by.text('Got It')).tap();
+  await element(by.text('Go home')).tap();
+};
+
 describe('Example', () => {
   beforeAll(async () => {
-    await device.launchApp();
+    await device.launchApp({
+      newInstance: true,
+    });
+
+    // When using the dev client and not reusing the app
+    await handleDevClient();
   });
 
   beforeEach(async () => {
